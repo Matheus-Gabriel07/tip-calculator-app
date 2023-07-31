1ba3a4b6296c68b763274a7e3b4f6b938da367ee
Matheus Gabriel <math.gsilva07@gmail.com>
2023-07-31 19:04:22 -0300

    fix: add variables in project and function

---
+++ script.js
@@ -0,0 +1,9 @@
+const inputBill  = document.querySelector("#numberBill").value;
+const inputPeople = document.querySelector("#nunberPeoples").value;
+
+//Buttons
+const tip05 = document.querySelector("#tip05");
+const tip10 = document.querySelector("#tip10");
+const tip15 = document.querySelector("#tip15");
+const tip25 = document.querySelector("#tip25");
+const tip50 = document.querySelector("#tip50");
